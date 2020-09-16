e45a09b06ae18e5e0895aa7d2173a3075250eea6
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-16 07:26:03 -0300

    :sparkles: feat: add if class and whileDeterminado calss

---
+++ src/estruturasDeControle/If.java
@@ -0,0 +1,27 @@
+package estruturasDeControle;
+
+import java.util.Scanner;
+
+public class If {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("informe a média: ");
+        double media = scanner.nextDouble();
+
+        if (media >= 7.0 && media <= 10) {
+            System.out.println("Aprovado! ");
+        }
+
+        if(media < 7 && media >= 4.5)
+            System.out.println("Recuperação");
+
+        if(media < 4.5 && media >= 0) {
+            System.out.println("Reprovado");
+        }
+
+        scanner.close();
+    }
+}
